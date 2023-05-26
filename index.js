15aef50e93235fb4cde73e57205428b98b4fd830
Bruno Henrique Crema <brunohcrema@gmail.com>
2023-05-26 13:21:53 -0300

    adding list the console

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-const $ = require('jquery')    
+const $ = require('jquery');
+const mm = require('music-metadata');
     
     function chooseMusic(){
         $('input').click();
+    }
+
+    function musicSelected(){
+        let files = $('input').get(0).files;
+        for (let i = 0; i < files.length; i++){
+            let {path} = files[i]
+            mm.parseFile(path, {native: true}).then(metadata =>{
+                console.log(metadata.common.title, metadata.common.artist, metadata.format.duration);
+            }
+        )
+        }
     }
